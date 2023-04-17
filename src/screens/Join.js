955be2e6a1ff3c22219716860ff1cc753d38044a
leejeawon92 <jeawon33333@gmail.com>
2023-04-17 23:50:32 +0900

    Join page layout

--- src/screens/Join.js
+++ src/screens/Join.js
@@ -1,8 +1,67 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const JoinMain = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`
+
+const JoinForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+  border: 0.8px solid gray;
+  border-radius: 15px;
+`
 
 function Join() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [name, setName] = useState('');
+
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+  const onPassword2Change = (event) => {
+    setPassword2(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('Email', email);
+    console.log('name', password);
+    console.log('Password', password);
+    console.log('Password2', password2);
+  }
   return (
     <>
-      회원가입페이지
+      <JoinMain>
+        <JoinForm onSubmit={onSubmit}>
+          <label>이메일</label>
+          <input type='email' value={email} onChange={onEmailChange} />
+          <label>이름</label>
+          <input type='text' value={name} onChange={onNameChange} />
+          <label>패스워드</label>
+          <input type='password' value={password} onChange={onPasswordChange} />
+          <label>패스워드 재입력</label>
+          <input type='password' value={password} onChange={onPassword2Change} />
+          <br />
+          <button formAction=''>Login</button>
+        </JoinForm>
+      </JoinMain>
     </>
   );
 }
